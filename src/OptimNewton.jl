a754495848d8f1258d176dfcd395a7e07aae84a3
FabianTamayo <tamayofabio0@gmail.com>
2019-03-25 11:47:50 -0400

    Add code

--- src/OptimNewton.jl
+++ src/OptimNewton.jl
@@ -1,5 +1,25 @@
 module OptimNewton
 
-greet() = print("Hello World!")
+function optimize_newton(x, f, g, h;
+                         grtol=1e-4,
+                         gatol=1e-4,
+                         prints=false)
+    while true
+        fx = f(x)
+        gx = g(x)
+        hx = h(x)
+        #convergence
+        abs(abs(gx) / fx) <= grtol && break
+        abs(gx) <= gatol && break
+
+        if prints
+            println("|G| = $(abs(gx))")
+        end
+
+        #move to next p
+        x -= gx / hx
+    end
+    return x
+end
 
 end # module
